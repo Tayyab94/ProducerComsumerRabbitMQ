f318caf3460cb979a5ada5b29bb2c5e6ac5aa3ee
tayyab bhatti <tayyab.bhatti30@hotmail.com>
2020-04-14 01:03:29 +0500

    First commit, Here we have just built Sender section of miscroservice.......... in AspNet Core

---
+++ Producer/Sender.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+   public class Sender
+    {
+       public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+                using(var channel =connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string Messsage = "Hello... Getting Started with .net Core  RabbitMQ..";
+
+                var body = Encoding.UTF8.GetBytes(Messsage);
+
+                channel.BasicPublish("", "BasicTest", null, body);
+
+                Console.WriteLine("Send MEssage.....{0}", Messsage);
+
+
+            }
+            Console.WriteLine("Press [Any Key ] tO  Exit Sender App..");
+            Console.ReadKey();
+        }
+    }
+}
